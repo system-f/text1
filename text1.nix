ce2e5c101043160d88c6e46d71e333d65138cc55
George Wilson <george@wils.online>
2018-02-03 01:41:00 +1000

    Update nix file to add setup-depends

--- text1.nix
+++ text1.nix
@@ -1,10 +1,12 @@
-{ mkDerivation, base, binary, directory, doctest, filepath, lens
-, papa, QuickCheck, semigroups, stdenv, template-haskell, text
+{ mkDerivation, base, binary, Cabal, cabal-doctest, directory
+, doctest, filepath, lens, papa, QuickCheck, semigroups, stdenv
+, template-haskell, text
 }:
 mkDerivation {
   pname = "text1";
   version = "0.0.5";
   src = ./.;
+  setupHaskellDepends = [ base Cabal cabal-doctest ];
   libraryHaskellDepends = [ base binary lens papa semigroups text ];
   testHaskellDepends = [
     base directory doctest filepath QuickCheck template-haskell
