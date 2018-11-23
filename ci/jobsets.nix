63de39d67cb8ae5bfa300e86da684ff577c72027
Dave Laing <dave.laing.80@gmail.com>
2018-11-23 10:22:05 +1000

    Bumps versions of nixpkgs and GHCs for Hydra

--- ci/jobsets.nix
+++ ci/jobsets.nix
@@ -18,7 +18,7 @@
             "keepnr": 5,
             "inputs": {
                 "text1": { "type": "git", "value": "https://github.com/qfpl/text1", "emailresponsible": false },
-                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.09", "emailresponsible": false }
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-18.09", "emailresponsible": false }
             }
         }
     }
